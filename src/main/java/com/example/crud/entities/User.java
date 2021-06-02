100eef1c0c699b36716c5378d852bb84ac1893e6
felipersumiya <felipersumiya@gmail.com>
2021-06-02 16:56:16 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/crud/entities/User.java
+++ src/main/java/com/example/crud/entities/User.java
@@ -2,87 +2,95 @@ package com.example.crud.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue (strategy  = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
+
 	public User() {
-		
+
 	}
 
-	
 	public User(Long id, String name, String email, String phone, String password) {
-		
+
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
-		
+
 	}
-
-
-
 
 	public Long getId() {
-		
+
 		return id;
-		
-	}
-	public void setId(Long id) {
-		
-		this.id = id;
-		
-	}
-	public String getName() {
-		
-		return name;
-		
-	}
-	public void setName(String name) {
-		
-		
-		this.name = name;
-		
-		
-	}
-	public String getEmail() {
-		
-		
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		
-		this.email = email;
-	}
-	
-	public String getPhone() {
-		
-		return phone;
-	}
-	public void setPhone(String phone) {
-		
-		this.phone = phone;
-		
-	}
-	public String getPassword() {
-		
-		return password;
-		
-	}
-	public void setPassword(String password) {
-		
-		this.password = password;
-		
+
 	}
 
+	public void setId(Long id) {
+
+		this.id = id;
+
+	}
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public void setName(String name) {
+
+		this.name = name;
+
+	}
+
+	public String getEmail() {
+
+		return email;
+	}
+
+	public void setEmail(String email) {
+
+		this.email = email;
+	}
+
+	public String getPhone() {
+
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+
+		this.phone = phone;
+
+	}
+
+	public String getPassword() {
+
+		return password;
+
+	}
+
+	public void setPassword(String password) {
+
+		this.password = password;
+
+	}
 
 	@Override
 	public int hashCode() {
@@ -91,7 +99,6 @@ public class User implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -109,7 +116,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
