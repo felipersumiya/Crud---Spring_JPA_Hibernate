1f7fef171a3d480a15d5b79a5ddf94b8a48a8bf0
felipersumiya <felipersumiya@gmail.com>
2021-06-03 15:44:05 -0300

    Subtotal e total

--- src/main/java/com/example/crud/entities/OrderItem.java
+++ src/main/java/com/example/crud/entities/OrderItem.java
@@ -76,6 +76,13 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		
+		return price * quantity;
+		
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
