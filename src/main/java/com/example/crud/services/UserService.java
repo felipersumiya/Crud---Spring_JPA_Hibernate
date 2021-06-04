4ca13c97f335241f371dad954e1a7a8d4eea4434
felipersumiya <felipersumiya@gmail.com>
2021-06-03 23:04:21 -0300

    Tratando exceção em delete()

--- src/main/java/com/example/crud/services/UserService.java
+++ src/main/java/com/example/crud/services/UserService.java
@@ -4,58 +4,66 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.crud.entities.User;
 import com.example.crud.repositories.UserRepository;
+import com.example.crud.services.exceptions.DatabaseException;
 import com.example.crud.services.exceptions.ResourceNotFoundException;
-
 
 @Service
 public class UserService {
-	
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	
-	public List<User> findAll(){
-			
+
+	public List<User> findAll() {
+
 		return userRepository.findAll();
-		
-		
+
 	}
-	
+
 	public User findById(Long id) {
-		
+
 		Optional<User> obj = userRepository.findById(id);
-	
+
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-		
+
 	}
-	
+
 	public User insert(User obj) {
-		
-		return userRepository.save(obj);	
+
+		return userRepository.save(obj);
+
 		
 	}
-	
+
 	public void delete(Long id) {
-		
-		userRepository.deleteById(id);
-		
+
+		try{
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+				
+				throw  new ResourceNotFoundException(id);
+				
+		}catch (DataIntegrityViolationException e) {
+				
+				throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
-		
+
 		User entity = userRepository.getOne(id);
-		updateData(entity,obj);
-		
+		updateData(entity, obj);
+
 		return userRepository.save(entity);
 	}
 
 	private void updateData(User entity, User obj) {
-		
+
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
---
+++ src/main/java/com/example/crud/services/exceptions/DatabaseException.java
@@ -0,0 +1,16 @@
+package com.example.crud.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public DatabaseException(String message) {
+		
+		super(message);
+		
+	}
+	
+	
+
+}
